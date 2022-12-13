a7083f8a10b293f2afe5c4ed3f2119a7fac54fb8
mbugua <mbuguafaimba@gmail.com>
2022-12-13 08:59:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
